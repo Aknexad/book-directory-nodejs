c94520fd95f6f103eb5062099d3237f461a3c3b8
Aknexad <aknejad@outlook.com>
2022-09-26 19:33:26 +0330

    add valadations

--- index.js
+++ index.js
@@ -7,40 +7,58 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
+  res.send('Home Page');
+});
+
+app.get('/api/books', (req, res) => {
   res.json(books);
 });
 
-app.get('/:id', (req, res) => {
-  const { id } = req.params;
-  const book = books.find((b) => b.id === id);
-  if (!book) res.status(404).send('Book dont exsist');
+app.get('/api/books/:id', (req, res) => {
+  const book = books.find((b) => b.id === req.params.id);
+  if (!book) return res.status(404).send('The Book With this ID Not Find');
   res.json(book);
 });
 
-app.post('/', (req, res) => {
-  const body = req.body;
-  let id = books.length + 1;
-  body.id = id.toString();
-  books.push(body);
-  console.log(body);
+app.post('/api/books', (req, res) => {
+  if (req.body.title === '' || req.body.aurth === '') {
+    res.status(400).res.send('invaled data');
+    return;
+  }
 
+  const book = {
+    id: (books.length + 1).toString(),
+    title: req.body.title,
+    aurth: req.body.aurth,
+  };
+
+  books.push(book);
+  res.json(book);
   res.send('Book has been added');
 });
 
-app.put('/:id', (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  const bookIndex = books.findIndex((b) => b.id === id);
-  books.splice(bookIndex, 1, body);
-  res.send('book updaated');
+app.put('/api/books/:id', (req, res) => {
+  const book = books.find((b) => b.id === req.params.id);
+  if (!book) return res.status(404).send('The Book With this ID Not Find');
+
+  if (req.body.title === '' || req.body.aurth === '') {
+    res.status(400).res.send('invaled data');
+    return;
+  }
+
+  book.title = req.body.title;
+  book.aurth = req.body.aurth;
+
+  res.json(book).send('book updated');
 });
 
-app.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  const bookIndex = books.findIndex((b) => b.id === id);
-  if (bookIndex === -1) res.send('Book dont exist');
-  books.splice(bookIndex, 1);
-  res.send('book deleted');
+app.delete('/api/books/:id', (req, res) => {
+  const book = books.find((b) => b.id === req.params.id);
+  if (!book) return res.status(404).send('The Book With this ID Not Find');
+
+  const index = books.indexOf(book);
+  books.splice(index, 1);
+  res.send('Book deleted');
 });
 
 const PORT = process.env.PORT || 3000;
